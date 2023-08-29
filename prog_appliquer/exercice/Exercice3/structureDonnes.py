85340b63641e92a0595fe545f0c762ebc1113a5e
PureLeaf <volteczx8@gmail.com>
2023-08-29 15:19:07 -0400

    Finition exercice 3 prog objet

--- prog_appliquer/exercice/Exercice3/structureDonnes.py
+++ prog_appliquer/exercice/Exercice3/structureDonnes.py
@@ -25,10 +25,28 @@ print(liste[-1])
 
 #2 de l'exercice
 def lexicaliser(chaine):
-    return chaine.split()
+    mots = chaine.split()
+    lexique = {}
+    for mot in mots:
+        if mot in lexique:
+            lexique[mot] += 1
+        else:
+            lexique[mot] = 1
+    return lexique
     
 
 lex = lexicaliser("Écrire une fonction lexicaliser ayant un argument (une chaîne de caractères) et qui renvoie un dictionnaire qui contient la fréquence de tous les mots de la chaîne entrée.")
 print(lex)
 
 #3 de l'exercice
+dictionnaire = {
+    "Au" : {
+        "Te/Tf" : [2970, 1063],
+        "Z/A" : [79, 196.967],
+    },
+    "Ga" : {
+        "Te/Tf" : [2237, 29.8],
+        "Z/A" : [31, 69.72],
+    }}
+
+print(dictionnaire["Ga"]["Z/A"][0])
