806aff17b4b48f0f83e80a0ea9a9507cd91ba1c2
ᑮᑘᖆᓬᒸᘍᗗᖴ <61324450+PureLeafGreen@users.noreply.github.com>
2023-10-03 11:49:52 -0400

    Avancement TP2p2

--- prog_appliquer/TP/TP2p2/TP2.py
+++ prog_appliquer/TP/TP2p2/TP2.py
@@ -4,13 +4,19 @@ import time
 # the input must be a number between -128 and 127 or a string with only capital letters and spaces
 buzzer = Buzzer(2)
 bit1 = LED(3)
+bit1On = False
 bit2 = LED(4)
+bit2On = False
 bit3 = LED(17)
+bit3On = False
 bit4 = LED(27)
 bit5 = LED(22)
 bit6 = LED(10)
 bit7 = LED(9)
 bit8 = LED(11)
+rgbRed = LED(16)
+rgbGreen = LED(20)
+rgbBlue = LED(21)
 error = True
 while error:
     userInput = input("Quelle est votre entrée? : ")
@@ -51,45 +57,80 @@ while error:
 print("Le texte est: " + userInput)
 
 #Encode the string if it only digits to binary to send it to the LEDs
-if userInput.isdigit():
-    userInput = int(userInput)
-    if userInput < 0:
+if userInput.isdigit() and int(userInput) > 0:
+    if int(userInput) >= 128:
         bit8.on()
-        userInput = userInput * -1
-    else:
-        bit8.off()
-    if userInput >= 64:
+        userInput = int(userInput) - 128
+    if int(userInput) >= 64:
         bit7.on()
-        userInput = userInput - 64
-    else:
-        bit7.off()
-    if userInput >= 32:
+        userInput = int(userInput) - 64
+    if int(userInput) >= 32:
         bit6.on()
-        userInput = userInput - 32
-    else:
-        bit6.off()
-    if userInput >= 16:
+        userInput = int(userInput) - 32
+    if int(userInput) >= 16:
         bit5.on()
-        userInput = userInput - 16
-    else:
-        bit5.off()
-    if userInput >= 8:
+        userInput = int(userInput) - 16
+    if int(userInput) >= 8:
         bit4.on()
-        userInput = userInput - 8
-    else:
-        bit4.off()
-    if userInput >= 4:
+        userInput = int(userInput) - 8
+    if int(userInput) >= 4:
         bit3.on()
-        userInput = userInput - 4
-    else:
-        bit3.off()
-    if userInput >= 2:
+        bit3On = True
+        userInput = int(userInput) - 4
+    if int(userInput) >= 2:
         bit2.on()
-        userInput = userInput - 2
-    else:
-        bit2.off()
-    if userInput >= 1:
+        bit2On = True
+        userInput = int(userInput) - 2
+    if int(userInput) >= 1:
         bit1.on()
-        userInput = userInput - 1
-    else:
-        bit1.off()
+        bit1On = True
+        userInput = int(userInput) - 1
+#if the number is negative, the program turn the 8th bit on and turn all the other bits to match the input
+elif int(userInput) < 0:
+    bit8.on()
+    userInput = int(userInput) + 128
+    if int(userInput) >= 64:
+        bit7.on()
+        userInput = int(userInput) - 64
+    if int(userInput) >= 32:
+        bit6.on()
+        userInput = int(userInput) - 32
+    if int(userInput) >= 16:
+        bit5.on()
+        userInput = int(userInput) - 16
+    if int(userInput) >= 8:
+        bit4.on()
+        userInput = int(userInput) - 8
+    if int(userInput) >= 4:
+        bit3.on()
+        bit3On = True
+        userInput = int(userInput) - 4
+    if int(userInput) >= 2:
+        bit2.on()
+        bit2On = True
+        userInput = int(userInput) - 2
+    if int(userInput) >= 1:
+        bit1.on()
+        bit1On = True
+        userInput = int(userInput) - 1
+time.sleep(3)
+bit1.off()
+bit2.off()
+bit3.off()
+bit4.off()
+bit5.off()
+bit6.off()
+bit7.off()
+bit8.off()
+
+#Use the 3 first bit to determine the color of the RGB LED, if all the 3 first bit are off, the LED is white
+rgbRed.on()
+rgbGreen.on()
+rgbBlue.on()
+if bit1On: 
+    rgbRed.off()
+if bit2On:
+    rgbGreen.off()
+if bit3On:
+    rgbBlue.off()
+time.sleep(3)
