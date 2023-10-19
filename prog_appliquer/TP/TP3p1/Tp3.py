af8c848aaa7a19db009038d5c39fb68a88537e46
ᑮᑘᖆᓬᒸᘍᗗᖴ <61324450+PureLeafGreen@users.noreply.github.com>
2023-10-18 20:40:45 -0400

    Tp3p1 finit

--- prog_appliquer/TP/TP3p1/Tp3.py
+++ prog_appliquer/TP/TP3p1/Tp3.py
@@ -3,8 +3,7 @@ from gpiozero import MotionSensor, DistanceSensor, RGBLED, Buzzer
 import adafruit_dht
 import psutil
 from time import sleep
-from signal import pause
-import turtle
+from turtle import *
 
 for proc in psutil.process_iter():
     if proc.name() == 'libgpiod_pulsein' or proc.name() == 'libgpiod_pulsei':
@@ -16,10 +15,11 @@ rgbLed = RGBLED(red=13, green=19, blue=26, active_high=False)
 humiditySensor = adafruit_dht.DHT11(21)
 buzzer = Buzzer(6)
 
-numberOfSeconds = 1
+numberOfSeconds = 0
 waitingSeconds = 1
 coleur = ""
 programStarted = False
+turtle = Turtle()
 
 
 #Turtle program that have one container which add 0.1 second in waitingSeconds, another container which remove 0.1 seconds in waitingSeconds and a last container which stop the program
@@ -40,44 +40,17 @@ def stopProgram():
     global programStarted
     programStarted = False
     print("Le programme est maintenant arrêté")
+    proc.kill()
 
-#Turtle program that have one square clickable which add 0.1 second in waitingSeconds with a label (+ 0.1 seconde), another square clickable which remove 0.1 seconds in waitingSeconds with a label (- 0.1 seconde) and a last sqaure clickable which stop the program with a label (Arret de la surveillance)
-turtle.setup(500, 500)
-turtle.title("Surveillance")
-turtle.bgcolor("black")
-turtle.color("white")
-turtle.speed(0)
-turtle.hideturtle()
-turtle.penup()
-turtle.goto(-200, 200)
-turtle.pendown()
-turtle.goto(200, 200)
-turtle.goto(200, -200)
-turtle.goto(-200, -200)
-turtle.goto(-200, 200)
-turtle.penup()
-turtle.goto(-100, 100)
-turtle.pendown()
-turtle.goto(100, 100)
-turtle.goto(100, -100)
-turtle.goto(-100, -100)
-turtle.goto(-100, 100)
-turtle.penup()
-turtle.goto(-50, 50)
-turtle.pendown()
-turtle.goto(50, 50)
-turtle.goto(50, -50)
-turtle.goto(-50, -50)
-turtle.goto(-50, 50)
-turtle.penup()
-turtle.goto(-100, 0)
-turtle.pendown()
-turtle.goto(100, 0)
-turtle.penup()
-turtle.goto(-100, -50)
-turtle.pendown()
-
-
+def getClick(x,y):
+    global WaitingSeconds
+    global numberOfSeconds
+    if x >= -450 and x <= -250 and y >= -100 and y <= 100:
+        addWaitingSeconds()
+    elif x >= -200 and x <= 0 and y >= -100 and y <= 100:
+        removeWaitingSeconds()
+    elif x >= 50 and x <= 250 and y >= -100 and y <= 100:
+        stopProgram()
 
 
 def program():
@@ -92,25 +65,26 @@ def program():
     global coleur
     programStarted = False
     while True:
+        sleep(1)
         print("Faite un mouvement pour commencer le programme")
         programStarted = motionDetector.wait_for_motion()
         while programStarted:
-            if (numberOfSeconds == 1):
+            if (numberOfSeconds == 0):
+                numberOfSeconds = 1
                 print("Merci, c'est parti!")
                 buzzer.beep(0.1, 0.1, 1)
             try:
                 temp = humiditySensor.temperature
                 humidity = humiditySensor.humidity
-                #distance in meter
                 distance = distanceSensor.distance
                 if distance * 100 < 10:
-                    rgbLed.color = (0, 1, 1)
+                    rgbLed.color = (1, 0, 0)
                     couleur = "rouge"
                 elif distance * 100 <= 30 and distance * 100 >= 10:
-                    rgbLed.color = (0, 0, 1)
+                    rgbLed.color = (1, 1, 0)
                     couleur = "jaune"
                 else:
-                    rgbLed.color = (1, 0, 1)
+                    rgbLed.color = (0, 1, 0)
                     couleur = "vert"
                 print("Seconde " + str(numberOfSeconds) + " : Temperature: {}° Humidity: {}% ".format(temp, humidity) + "Distance: {} m".format(distance) + " La RGBLED est " + couleur)
             except RuntimeError as error:
@@ -124,9 +98,57 @@ def program():
         if programStarted == False:
             break
 
+turtle.color("black")
+turtle.speed(0)
+turtle.hideturtle()
+turtle.penup()
+#Draw a rectangle on the middle left of the screen
+turtle.goto(-450, 0)
+for i in range(4):
+    turtle.pendown()
+    if i % 2 == 0:
+        turtle.forward(200)
+    else:
+        turtle.forward(100)
+    turtle.right(90)
+    turtle.penup()
+#Write label in the middle of the square
+turtle.penup()
+turtle.goto(-425, -50)
+turtle.write("+ 0.1 seconde", font=("Arial", 20, "normal"))
+
+turtle.goto(-200, 0)
+for i in range(4):
+    turtle.pendown()
+    if i % 2 == 0:
+        turtle.forward(200)
+    else:
+        turtle.forward(100)
+    turtle.right(90)
+    turtle.penup()
+#Write label in the middle of the square
+turtle.penup()
+turtle.goto(-175, -50)
+turtle.write("- 0.1 seconde", font=("Arial", 20, "normal"))
+
+turtle.goto(50, 0)
+for i in range(4):
+    turtle.pendown()
+    if i % 2 == 0:
+        turtle.forward(200)
+    else:
+        turtle.forward(100)
+    turtle.right(90)
+    turtle.penup()
+#Write label in the middle of the square
+turtle.penup()
+turtle.goto(65, -75)
+turtle.write("Arreter la \r surveillance", font=("Arial", 20, "normal"))
+onscreenclick(getClick,1)
+
 #Start program as a thread and start turtle program
 thread = Thread(target=program)
 thread.start()
-turtle.mainloop()
+mainloop()
 
 
