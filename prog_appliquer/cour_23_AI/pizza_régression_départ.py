c4952c43b3d2231c8cca48d9251c442543802df9
pureleaf <volteczx8@gmail.com>
2023-11-14 14:02:45 -0500

    cour du 14 novembre terminer

--- prog_appliquer/cour_23_AI/pizza_régression_départ.py
+++ prog_appliquer/cour_23_AI/pizza_régression_départ.py
@@ -6,17 +6,35 @@ def tracerDonnees(X,Y):
     plt.scatter(X, Y, label = 'Données')  
 # Prédictions de Y à partir des X pour un modèle (m,b) 
 def predire(X, m, b):
-    pass
+    Y = m * X + b
+    return Y
 # Erreur (perte) moyenne pour un modèle (m,b)
 def perte(X, Y, m, b): # moyenne des pertes entre un modèle (m,b) et la réalité Y
-    pass   
+    predictionY = predire(X, m, b)
+    return np.average((predictionY - Y) ** 2)
 # Entraînement pour trouver un modèle (m,b)
 def entraine(X, Y, iterations, ta): # ta = taux d'apprentissage
                                     # iterations: pour éviter les vas-et-vient infinis
                                     # but: trouver m et b
-    pass
+    m = b = 0
+    for i in range(iterations):
+        perteCourant = perte(X, Y, m, b)
+        print("Iteration " + str(i) + " : m = " + str(m) + " b = " + str(b) + " perte = " + str(perteCourant))
+        # On essaie de trouver un meilleur modèle
+        if perte(X,Y,m - ta, b) < perteCourant:
+            m = m - ta
+        elif perte(X,Y,m + ta, b) < perteCourant:
+            m = m + ta
+        elif perte(X,Y,m, b - ta) < perteCourant:
+            b = b - ta
+        elif perte(X,Y, m, b + ta) < perteCourant:
+            b = b + ta
+        else:
+            return m, b
+    print("Nb iterations atteint, pt l'augmenter")
+    return m, b
 if __name__ == "__main__":
-    # Importer les données dans deux tableaux distincts (unpack)
+    # Importer les données dans deux tableaux distinc ts (unpack)
     X, Y = np.loadtxt("pizza.txt", skiprows=1, unpack=True)
     tracerDonnees(X,Y)
     plt.show()                                 
